9b101b6665807dcc62b8f4991362beb3a23cc890
Snyamongo <Snyamongo@safaricom.co.ke>
2023-07-24 10:10:50 +0300

    Create users page

---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Users",
+};
+
+import React from "react";
+
+export default function UsersPage() {
+  return <div>page</div>;
+}
